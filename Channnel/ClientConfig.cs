ecd23458779f2cd2d9d7302b041b4d4ef057ad2f
Vishal Anand <vishal_24_anand@hotmail.com>
2016-12-03 12:33:04 +0530

    Removed client config

--- Channnel/ClientConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Channnel
-{
-    public class ClientConfig
-    {
-        public List<InvocationScope> InvocationScopes = new List<InvocationScope>();
-    }
-}
